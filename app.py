efa94511350ee077eca71a37398f0dbdad2802f3
tonytregcdw <54070632+tonytregcdw@users.noreply.github.com>
2025-09-16 15:20:56 +0100

    initial

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()  # This will read variables from .env
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+TEST_ENV_VAR1 = os.environ.get('TEST_ENV_VAR1')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hello, world! {SECRET_KEY} {TEST_ENV_VAR1}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
